697dd76226dd79d39f448106e9b1b1a8a2c00bff
Aleksandr Hemingway <deepspaceghost@protonmail.com>
2023-11-21 14:42:18 -0500

    Update main.py
    Updated the build number.

--- code/main.py
+++ code/main.py
@@ -10,7 +10,7 @@ class Game():
         # general setup
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption("Voyager (v2023.11.20.0.11.0)")
+        pygame.display.set_caption("Voyager (v2023.11.21.0.11.1)")
         self.clock = pygame.time.Clock()
 
         self.level = Level()
